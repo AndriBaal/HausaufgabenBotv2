c71beceadbe050c3a6866437f2a6a91cccacc881
Andri <80914617+Shuray04@users.noreply.github.com>
2021-09-13 11:34:47 +0200

    [Update] Auto removing Homework

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from os import error
 import discord
 import json
 from discord.ext import commands, tasks
-from datetime import date
+import datetime
 
 bot = commands.Bot(command_prefix='/')
 homework = []
@@ -19,7 +19,7 @@ async def command_info(ctx):
     await ctx.channel.send(' - /homework (Zeigt alle Hasuaufgaben an)')
     await ctx.channel.send(' - /addHomework modul datum beschreibung')
     await ctx.channel.send(' - /removeHomework index')
-    await ctx.channel.send('Beispiel:   /addHomework 117 2021-15-09' + ' "Netzwerkdiagramm zeigen"')
+    await ctx.channel.send('Beispiel:   /addHomework 117 2021-09-15' + ' "Netzwerkdiagramm zeigen"')
 
 @bot.command(name='addHomework')
 async def command_add_homework(ctx, module=None, termination_date=None, description=None):
@@ -62,5 +62,15 @@ async def on_command_error(ctx, error):
     print(error)
     await ctx.channel.send('Unknown command. Get help with /info') 
 
-token = ''
+@tasks.loop(hours=1)
+async def mytask():
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    for i in range(len(homework)):
+        if tomorrow > datetime.datetime.strptime(homework[i]['termination_date'], '%Y-%m-%d').date():
+            homework.pop(i)
+    write_data()
+    
+mytask.start()
+
+token = 'ODg2ODU3MTUxNzY1Njg4Mzgz.YT7sFQ.-Aonv0QpKbiDaaLjO6G_cfnr2bA'
 bot.run(token)
--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /home/ubuntu/hausaufgaben_v2
 tmux new-session -d -s bot \; send-keys "python3 /home/ubuntu/hausaufgaben_v2/main.py" Enter
 tmux attach-session -t bot
